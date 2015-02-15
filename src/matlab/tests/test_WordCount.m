350f28eb932749e6354b555076fbaef8b45afd22
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-02-15 20:23:34 +0100

    TST: unit test for WordCount

---
+++ src/matlab/tests/test_WordCount.m
@@ -0,0 +1,9 @@
+function test_suite=test_WordCount()
+    % requires MOxUnit or Matlab xUnit
+    % see https://github.com/nno/MOxUnit
+    initTestSuite
+      
+
+function test_test_WordCount_basics()      
+    assertEqual(WordCount('Hi Ya  |  Freak ','|'),2);
+    assertEqual(WordCount('Hi Ya    Freak '),3);
