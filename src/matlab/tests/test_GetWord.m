5fb1c61f41646249dc0b35c0aac1df51476dd994
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-02-16 11:35:56 +0100

    TST: more test cases for GetWord

--- src/matlab/tests/test_GetWord.m
+++ src/matlab/tests/test_GetWord.m
@@ -2,21 +2,43 @@ function test_suite=test_GetWord()
     % requires MOxUnit or Matlab xUnit
     % see https://github.com/nno/MOxUnit
     initTestSuite
-      
 
-function test_GetWord_basics()      
+
+function test_GetWord_basics()
     [err,w]=GetWord ('Hello Jim | Munch',2);
     assertEqual(err,0);
     assertEqual(w,'Jim');
-    
+
     [err,w]=GetWord ('Hello Jim | Munch',2,'|');
     assertEqual(err,0);
     assertEqual(w,' Munch');
-    
+
     [err,w]=GetWord ('Hello Jim | Munch',2,'| l');
     assertEqual(err,0);
     assertEqual(w,'o');
-    
+
+
+    [err,w]=GetWord ('  a  bl c| l d e',1,'| l');
+    assertEqual(err,0);
+    assertEqual(w,'a');
+
+    [err,w]=GetWord ('  a  bl c| l d e',2,'| l');
+    assertEqual(err,0);
+    assertEqual(w,'b');
+
+    [err,w]=GetWord ('  a  bl c| l d e',3,'| l');
+    assertEqual(err,0);
+    assertEqual(w,'c');
+
+    [err,w]=GetWord ('  a  bl c| l d e',4,'| l');
+    assertEqual(err,0);
+    assertEqual(w,'d');
+
+    [err,w]=GetWord ('  a  bl c| l d e',5,'| l');
+    assertEqual(err,0);
+    assertEqual(w,'e');
+
+
     [err,w]=GetWord ('Hello Jim | Munch',2,'x');
     assertEqual(err,1);
     assertEqual(w,'');
--- src/matlab/tests/test_WordCount.m
+++ src/matlab/tests/test_WordCount.m
@@ -2,8 +2,8 @@ function test_suite=test_WordCount()
     % requires MOxUnit or Matlab xUnit
     % see https://github.com/nno/MOxUnit
     initTestSuite
-      
 
-function test_test_WordCount_basics()      
+
+function test_test_WordCount_basics()
     assertEqual(WordCount('Hi Ya  |  Freak ','|'),2);
     assertEqual(WordCount('Hi Ya    Freak '),3);
